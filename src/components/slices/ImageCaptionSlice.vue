b6e619b19fa5f793d2ab6180994453b346c52332
Levi Gable <levimykel@gmail.com>
2019-02-13 13:19:23 +0100

    Add more image templates

--- src/components/slices/ImageCaptionSlice.vue
+++ src/components/slices/ImageCaptionSlice.vue
@@ -1,9 +1,26 @@
 <template>
-  <div class='post-part single container'>
-    <p class="block-img" :class="size">
-      <prismic-image :field="img"/>
-    </p>
-    <blockquote class="image-label">{{ $prismic.richTextAsPlain(caption) }}</blockquote>
+  <div>
+    <template v-if="size === 'image-full-width'">
+      <div class='blog-header single' :style="{ 'background-image': 'url(' + img.url + ')'}">
+        <template v-if="$prismic.richTextAsPlain(caption) != ''">
+          <div class="wrapper">
+            <h1>{{ $prismic.richTextAsPlain(caption) }}</h1>
+          </div>
+        </template>
+      </div>
+    </template>
+    <template v-else>
+      <div class='post-part single container'>
+        <p class="block-img" :class="size">
+          <prismic-image :field="img"/>
+        </p>
+        <template v-if="$prismic.richTextAsPlain(caption) != ''">
+          <p>
+            <span class="image-label">{{ $prismic.richTextAsPlain(caption) }}</span>
+          </p>
+        </template>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -15,6 +32,50 @@ export default {
 </script>
 
 <style scoped>
+.blog-header {
+  height: 85px;
+  position: relative;
+  font-family: 'Lato', sans-serif;
+  font-weight: 400;
+  background-color: white;
+  background-size: cover;
+  color: white;
+  margin-bottom: 3rem;
+}
+.blog-header.single::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.2);
+}
+.blog-header.single::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: -moz-linear-gradient(to bottom, rgba(255,255,255,0) 0%, rgba(0,0,0,0) 1%, rgba(0,0,0,0.05) 80%, rgba(0,0,0,0.1) 90%, rgba(0,0,0,0.2) 100%);
+  background: -webkit-linear-gradient(to bottom, rgba(255,255,255,0) 0%, rgba(0,0,0,0) 1%, rgba(0,0,0,0.05) 80%, rgba(0,0,0,0.1) 90%, rgba(0,0,0,0.2) 100%);
+  background: -o-linear-gradient(to bottom, rgba(255,255,255,0) 0%, rgba(0,0,0,0) 1%, rgba(0,0,0,0.05) 80%, rgba(0,0,0,0.1) 90%, rgba(0,0,0,0.2) 100%);
+  background: -ms-linear-gradient(to bottom, rgba(255,255,255,0) 0%, rgba(0,0,0,0) 1%, rgba(0,0,0,0.05) 80%, rgba(0,0,0,0.1) 90%, rgba(0,0,0,0.2) 100%);
+  background: linear-gradient(to bottom, rgba(255,255,255,0) 0%, rgba(0,0,0,0) 1%, rgba(0,0,0,0.05) 80%, rgba(0,0,0,0.1) 90%, rgba(0, 0, 0, 0.2) 100%);
+}
+.blog-header.single, .blog-header.home {
+  height: 400px;
+}
+.blog-header .wrapper {
+  text-align: center;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  -ms-transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
 .post-part.single .image-full-width + .image-label {
   width: 100%;
 }
@@ -24,6 +85,9 @@ export default {
 }
 .post-part.single .image-full-width + .image-label, .blog-main.single .image-full-width + .image-label {
   width: 100%;
+}
+.post-part.single .block-img {
+  margin-bottom: 24px;
 }
 .post-part.single .image-label {
   display: block;
@@ -46,6 +110,12 @@ export default {
 @media (max-width: 767px) {
   .container {
     padding: 0 20px;
+  }
+  .blog-header {
+    padding: 5px;
+  }
+  .blog-header .wrapper {
+    width: 80%;
   }
   .post-part, .blog-main {
     font-size: 18px;
