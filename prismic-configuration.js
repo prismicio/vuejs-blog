ff1c276c9e9d9b28f52bdc42552d73aaf35b03d8
Philip Snow <philipsnow@Philips-MacBook-Pro.local>
2019-03-08 16:11:59 +0100

    build line + prismic config

---
+++ prismic-configuration.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  apiEndpoint: 'https://your-repo-name.prismic.io/api',
+
+  // -- Access token if the Master is not open
+  // accessToken: 'xxxxxx',
+
+  // OAuth
+  // clientId: 'xxxxxx',
+  // clientSecret: 'xxxxxx',
+}
