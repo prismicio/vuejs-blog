08da4d8749510f8f751b0f4111b53844c1e3b540
Vincent Velociter <vincent.velociter@gmail.com>
2020-07-03 10:48:29 +0200

    Update dependencies

--- src/views/Preview.vue
+++ src/views/Preview.vue
@@ -7,12 +7,22 @@
 <script>
 import Vue from 'vue'
 
+/* export default { */
+/*   name: 'Preview', */
+/*   beforeCreate () { */
+/*     const previewToken = this.$route.query.token */
+
+/*     this.$prismic.client.previewSession(previewToken, this.$prismic.linkResolver, '/') */
+/*       .then((url) => { */
+/*         window.location.replace(url) */
+/*       }) */
+/*   } */
+/* } */
 export default {
   name: 'Preview',
   beforeCreate () {
-    const previewToken = this.$route.query.token
-
-    this.$prismic.client.previewSession(previewToken, this.$prismic.linkResolver, '/')
+    const { token, documentId } = this.$route.query
+    this.$prismic.client.getPreviewResolver(token, documentId).resolve(this.$prismic.linkResolver, '/')
       .then((url) => {
         window.location.replace(url)
       })
